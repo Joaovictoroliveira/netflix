fdd7f8477f73d1e0bf02a09fe1be74eab49b3654
Joao Victor <joao.gomes31@fatec.sp.gov.br>
2020-04-20 20:53:14 -0300

    Adicionando nova area na pagina index

--- netflix/scripts/script.js
+++ netflix/scripts/script.js
@@ -1,6 +1,6 @@
 function validacaoIndex() {
     if(document.querySelector("input").value == "") {
-        document.getElementById("email").style.border = "solid #ff6600";
+        document.getElementById("email").style.border = ".5px solid #ff6600";
         document.getElementById("email-validation").style.display = "block";
     }
 }
